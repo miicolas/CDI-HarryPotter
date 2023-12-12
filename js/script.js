be04c71dc517175722f2547580eae5dac14fe9ad
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-12-12 16:42:57 +0100

    ⚙️

--- js/script.js
+++ js/script.js
@@ -1,123 +1,142 @@
 // Filter function
 function filterType() {
-    const filterAll = document.getElementById("filterAll");
-    const filterGryffondor = document.getElementById("filterGryffondor");
-    const filterPoufsouffle = document.getElementById("filterPoufsouffle");
-    const filterSerdaigle = document.getElementById("filterSerdaigle");
-    const filterSerpentard = document.getElementById("filterSerpentard");
+  const filterAll = document.getElementById("filterAll");
+  const filterGryffondor = document.getElementById("filterGryffondor");
+  const filterPoufsouffle = document.getElementById("filterPoufsouffle");
+  const filterSerdaigle = document.getElementById("filterSerdaigle");
+  const filterSerpentard = document.getElementById("filterSerpentard");
 
-    filterAll.addEventListener("click", function () {
-        filterCards("Tous");
-    });
-    filterGryffondor.addEventListener("click", function () {
-        filterCards("Gryffondor");
-    });
-    filterPoufsouffle.addEventListener("click", function () {
-        filterCards("Poufsouffle");
-    });
-    filterSerdaigle.addEventListener("click", function () {
-        filterCards("Serdaigle");
-    });
-    filterSerpentard.addEventListener("click", function () {
-        filterCards("Serpentard");
-    });
+  filterAll.addEventListener("click", function () {
+    filterCards("Tous");
+  });
+  filterGryffondor.addEventListener("click", function () {
+    filterCards("Gryffondor");
+  });
+  filterPoufsouffle.addEventListener("click", function () {
+    filterCards("Poufsouffle");
+  });
+  filterSerdaigle.addEventListener("click", function () {
+    filterCards("Serdaigle");
+  });
+  filterSerpentard.addEventListener("click", function () {
+    filterCards("Serpentard");
+  });
 }
 function filterCards(selectedType) {
-    const galleryItems = document.querySelectorAll(".gallery-item");
-    galleryItems.forEach(function (item) {
-        const itemType = item.getAttribute("data-type");
-        if (selectedType === "Tous" || selectedType === itemType) {
-            item.style.display = "block";
-        } else {
-            item.style.display = "none";
-        }
-    });
+  const galleryItems = document.querySelectorAll(".gallery-item");
+  galleryItems.forEach(function (item) {
+    const itemType = item.getAttribute("data-type");
+    if (selectedType === "Tous" || selectedType === itemType) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
 
 function updateFilterButtons() {
-    const filterButtons = [filterAll, filterGryffondor, filterPoufsouffle, filterSerdaigle, filterSerpentard]
+  const filterButtons = [
+    filterAll,
+    filterGryffondor,
+    filterPoufsouffle,
+    filterSerdaigle,
+    filterSerpentard,
+  ];
 
-    filterButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            filterButtons.forEach(function(btn) {
-                btn.classList.remove('select');
-                btn.classList.add('unselect');
-            });
-            button.classList.add('select');
-            button.classList.remove('unselect');
-        });
+  filterButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      filterButtons.forEach(function (btn) {
+        btn.classList.remove("select");
+        btn.classList.add("unselect");
+      });
+      button.classList.add("select");
+      button.classList.remove("unselect");
     });
+  });
 }
-
 
 // Search function
 function searchCard(selectedType) {
-    const searchInput = document.getElementById("search");
-    const galleryItems = document.querySelectorAll(".gallery-item");
-    searchInput.addEventListener("input", function () {
-        const searchValue = searchInput.value.toLowerCase();
-        galleryItems.forEach(function (item) {
-            const name = item.querySelector("p").textContent.toLowerCase();
-            const itemType = item.getAttribute("data-type");
-            if (name.includes(searchValue)) {
-                item.style.display = "block";
-            } else {
-                item.style.display = "none";
-            }
-        });
+  const searchInput = document.getElementById("search");
+  const galleryItems = document.querySelectorAll(".gallery-item");
+  searchInput.addEventListener("input", function () {
+    const searchValue = searchInput.value.toLowerCase();
+    galleryItems.forEach(function (item) {
+      const name = item.querySelector("p").textContent.toLowerCase();
+      const itemType = item.getAttribute("data-type");
+      if (name.includes(searchValue)) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
     });
+  });
 }
 
 // CardDetails function
-function CardDetails (){
-    const galleryItems = document.querySelectorAll(".gallery-image");
-    galleryItems.forEach(function (item) {
-        item.addEventListener("click", function () {
-            const cardid = item.dataset.id ;
-            window.location.href = `///Users/nicolasbecharat/Documents/GitHub/harrypotter/cartes/${cardid}.html`
-        });
+function CardDetails() {
+  const galleryItems = document.querySelectorAll(".gallery-image");
+  galleryItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      const cardid = item.dataset.id;
+      window.location.href = `///Users/nicolasbecharat/Documents/GitHub/harrypotter/cartes/${cardid}.html`;
     });
+  });
 }
 
-function buttonAnimation (){
-    const scrollTo = document.getElementById("bouncebutton");
-    scrollTo.addEventListener("click", function () {
-        window.scrollBy ({
-            top : 40,
-            behavior : "smooth"
-        });
+function buttonAnimation() {
+  const scrollTo = document.getElementById("bouncebutton");
+  scrollTo.addEventListener("click", function () {
+    window.scrollBy({
+      top: 40,
+      behavior: "smooth",
     });
+  });
 }
 
 // Call functions
 document.addEventListener("DOMContentLoaded", function () {
-    filterType();
-    searchCard();
-    CardDetails();
-    updateFilterButtons();
-    buttonAnimation();
+  filterType();
+  searchCard();
+  CardDetails();
+  updateFilterButtons();
+  buttonAnimation();
 });
 
-const bounceContainerButton = document.querySelector('.bounce-container');
+const bounceContainerButton = document.querySelector(".bounce-container");
 
-bounceContainerButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    const tirageSection = document.getElementById('tirage');
-    tirageSection.scrollIntoView({ behavior: 'smooth' });
+bounceContainerButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const tirageSection = document.getElementById("tirage");
+  tirageSection.scrollIntoView({ behavior: "smooth" });
 });
 
-const heroButton = document.querySelector('.hero-button');
-heroButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    const gallerySection = document.getElementById('gallery-section');
-    gallerySection.scrollIntoView({ behavior: 'smooth' });
+const heroButton = document.querySelector(".hero-button");
+heroButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const gallerySection = document.getElementById("gallery-section");
+  gallerySection.scrollIntoView({ behavior: "smooth" });
 });
 
-const likeButton = document.querySelectorAll('.like-button');
-likeButton.forEach(function(button) {
-    button.addEventListener('click', function() {
-        button.classList.toggle('like-button--active');
-    }
-)
-     
+const likeButton = document.querySelectorAll(".like-button");
+likeButton.forEach(function (button) {
+  button.addEventListener("click", function () {
+    button.classList.toggle("like-button--active");
+  });
 });
+
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+const overlay = document.querySelector('.overlay');
+
+    menuIcon.addEventListener('click', () => {
+      overlay.style.display = 'block';
+      menuIcon.style.display = 'none';
+      closeIcon.style.display = 'block';
+    });
+
+    closeIcon.addEventListener('click', () => {
+      overlay.style.display = 'none';
+      closeIcon.style.display = 'none';
+      menuIcon.style.display = 'block';
+    });
