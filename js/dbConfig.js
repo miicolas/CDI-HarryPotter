6a7218e600855529e5dd5843eac21526b17fb127
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-12-18 17:18:31 +0100

    ⚙️

---
+++ js/dbConfig.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  socketPath: process.env.DB_SOCKET_PATH,
+});
+
+module.exports = pool;
---
+++ routes/login.js
@@ -0,0 +1,33 @@
+// routes/login.js
+const express = require("express");
+const bcrypt = require("bcrypt");
+const { query } = require("../js/queries");
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const result = await query("SELECT * FROM account WHERE username = ?", [
+      username,
+    ]);
+
+    if (result.length === 0) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    const hashedPassword = result[0].password;
+
+    const validPassword = await bcrypt.compare(password, hashedPassword);
+
+    if (!validPassword) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error logging in" });
+  }
+});
+
+module.exports = router;
