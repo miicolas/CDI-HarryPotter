2ab191a403be2f97e6452ef25bb58e7979c5bb31
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-12-23 18:28:21 +0100

    Fix image source path in profil.ejs

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const loginController = require("../controllers/loginController"); 
 const validateLogin = require("../middleware/loginValidation");
 
-router.post("/login", validateLogin,loginController.login); // Utilisez la fonction de contrôleur pour gérer la route
+router.post("/login", validateLogin,loginController.login); 
 
 module.exports = router;
