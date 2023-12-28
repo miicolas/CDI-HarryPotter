9f8505a48e3296301e15e8198d2574447f5e7625
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-12-28 11:29:46 +0100

    Update profilController.js

--- backend/controllers/profilController.js
+++ backend/controllers/profilController.js
@@ -1,25 +1,33 @@
-// controllers/profilController.js
 const { query } = require("../config/queries");
 
 async function getProfil(req, res) {
   try {
-    const userId = req.user.id // Récupération de l'id de l'utilisateur depuis le token
-    
+    const userId = req.user.id;
 
-    const result = await query("SELECT username, name FROM account WHERE id = ?", [userId]);
-
-    if (result.length === 0) {
-      res.redirect('login');
+    const userInfo = await query("SELECT username, name FROM account WHERE id = ?", [userId]);
+    if (userInfo.length === 0) {
+      return res.redirect('login');
     }
 
-    res.render ('profil', {username: result[0].username, name: result[0].name});
+    const cards_user = await query("SELECT * FROM cards JOIN UsersCards ON cards.id_card = UsersCards.id_card WHERE id_user = ?", [userId]);
+
+    let message = ''; 
+
+    if (cards_user.length === 0) {
+      // If the user has no cards, set the message
+      message = "Vous n'avez pas encore de cartes";
+    }
+
+    res.render('profil', {
+      username: userInfo[0].username,
+      name: userInfo[0].name,
+      message: message, 
+      cards: cards_user 
+    });
 
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Error signing up" });
+    res.status(500).json({ error: "Erreur lors de la récupération du profil de l'utilisateur" });
   }
 }
-
-module.exports = {
-  getProfil,
-};
+module.exports = {   getProfil, };
