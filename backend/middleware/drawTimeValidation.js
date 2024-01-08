258306d525150dd287821309d93dba26b14eb67c
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-01-08 19:40:08 +0100

    ⚙️

--- backend/middleware/drawTimeValidation.js
+++ backend/middleware/drawTimeValidation.js
@@ -6,22 +6,29 @@ async function drawTime(req, res, next) {
     const userId = req.user.id;
     const lastDrawTime = await query( "SELECT lastDraw FROM account WHERE id = ?", [userId]);
     const currentTime = Date.now(); // Heure actuelle en millisecondes
+    // console.log(lastDrawTime);
 
-    if (lastDrawTime.length > 0) { // Si lastDrawTime est défini dans les cookies
+    if (lastDrawTime !== null && lastDrawTime !== undefined) { // Si lastDrawTime est défini et différent de null
 
       const elapsedTime = currentTime - lastDrawTime[0].lastDraw; // Temps écoulé depuis le dernier tirage en millisecondes
       const twentyFourHoursInMillis = 24 * 60 * 60 * 1000;  // 24 heures en millisecondes
 
+      // console.log("elapsedTime = ", elapsedTime);
+      // console.log("twentyFourHoursInMillis = ", twentyFourHoursInMillis);
+      // const result = twentyFourHoursInMillis - elapsedTime;
+      // console.log("result = ", result);
       if (elapsedTime < twentyFourHoursInMillis) { // Si le temps écoulé est inférieur à 24 heures
         return res
           .status(403)
           .send(
             "Vous ne pouvez pas tirer de nouvelles cartes pour le moment. Attendez 24 heures."
           );
+
       }
     } else {
-      // Si lastDrawTime n'est pas défini dans les cookies, c'est la première fois que l'utilisateur tire des cartes
-      await query("UPDATE account SET firstDraw = true WHERE id = ?", [userId]);
+      // Si lastDrawTime n'est pas défini, c'est la première fois que l'utilisateur tire des cartes
+      await query("UPDATE account SET firstDraw = true WHERE id = ?", [userId]);  
+      console.log("firstDraw = true");
 
     }
 
